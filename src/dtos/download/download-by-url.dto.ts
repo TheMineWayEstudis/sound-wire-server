1e1fa6f7521d8d12b8b0e08cf0e35b1bf56f7b18
TheMineWay <joelcamposoliva9000@gmail.com>
2024-05-07 20:30:47 +0200

    Add validation

--- src/dtos/download/download-by-url.dto.ts
+++ src/dtos/download/download-by-url.dto.ts
@@ -1,3 +1,8 @@
+import { IsString, IsUrl, MaxLength } from 'class-validator';
+
 export class DownloadByUrlDTO {
+  @IsString()
+  @IsUrl({ host_whitelist: ['music.youtube.com', 'youtube.com'] })
+  @MaxLength(256)
   url: string;
 }
