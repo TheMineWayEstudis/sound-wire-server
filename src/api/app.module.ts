85bc91c7b2d01f5d76c3d691d0d39b8e45805fae
TheMineWay <joelcamposoliva9000@gmail.com>
2024-05-07 20:39:37 +0200

    Secure with API KEY

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
 import { DownloadModule } from './download/download.module';
+import { ApiKeyGuard, ApiKeysModule } from 'nestjs-api-keys';
+import { ENV } from '../constants/env/env.constant';
+import { APP_GUARD } from '@nestjs/core';
+
+const GLOBAL_PERMISSION = 'download.all';
 
 @Module({
-  imports: [DownloadModule],
+  imports: [
+    ApiKeysModule.register({
+      apiKeys: [
+        {
+          name: 'For downloading',
+          keys: [ENV.API_KEY],
+          permissions: [GLOBAL_PERMISSION],
+        },
+      ],
+    }),
+    DownloadModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ApiKeyGuard({ permissions: [GLOBAL_PERMISSION] }),
+    },
+  ],
 })
 export class AppModule {}
